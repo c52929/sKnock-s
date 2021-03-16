36a948e45bb6c1e54464a40565c256c00663eeff
#c52929 <tableknocker0@gmail.com>
2021-03-16 17:34:06 +0900

    Updated Kobun vocab

--- Kobun/js/mainMultiple.js
+++ Kobun/js/mainMultiple.js
@@ -5,16 +5,12 @@
 	let rs, selects, qNum, selected, jokyo, mode;
 
 	function reset(){
-		if(mode=='kd'){
-			words=['<ruby><rb>顕</rb><rt>あら</rt></ruby>はなり','あからさまなり','<ruby><rb>頑</rb><rt>かたく</rt></ruby>ななり','<ruby><rb>徒</rb><rt>あだ</rt></ruby>なり','<ruby><rb>漫</rb><rt>すず</rt></ruby>ろなり','無下なり'];
-			meanings=['まる見えだ/明らかだ','ちょっとの間だ','情趣を解さない/見苦しい','はかない/不誠実だ','あてもない/思いがけない/むやみに','まったくひどい/むやみに/まったく(～ない)'];
-		}else if(mode=='n'){
-			words=['公','<ruby><rb>際</rb><rt>きは</rt></ruby>','<ruby><rb>験</rb><rt>しるし</rt></ruby>・<ruby><rb>徴</rb><rt>しるし</rt></ruby>','<ruby><rb>理</rb><rt>ことわり</rt></ruby>','<ruby><rb>例</rb><rt>ためし</rt></ruby>','業','覚え','心地','<ruby><rb>現</rb><rt>うつつ</rt></ruby>','つとめて','<ruby><rb>晦</rb><rt>つごもり</rt></ruby>・<ruby><rb>晦日</rb><rt>つごもり</rt></ruby>','ついで','頼り・便り','志','<ruby><rb>由</rb><rt>よし</rt></ruby>','万','手','<ruby><rb>才</rb><rt>ざえ</rt></万>'];
-			meanings=['朝廷/天皇','身分/程度','御利益/効果/前兆','道理','例/語り草','仏事/こと','評判/寵愛(を受けること)','気持ち/病気','現実/正気','早朝/翌朝','月末','順序/機会','頼れるもの/縁故/便宜/機会','愛情/贈り物','縁/方法/由緒・風情/事情/そぶり','さまざまなこと/すべて','筆跡/演奏法','学問'];
-		}else{
-			words=['<ruby><rb>自</rb><rt>おのづか</rt></ruby>ら','即ち・則ち','<ruby><rb>疾</rb><rt>と</rt></ruby>く','いつしか','なべて','いとど','かく','とかく','しか','さ','さばかり','さながら','つゆ～打消','よも～じ','なかなか','さすがに','かつ','など・などか','なでふ・なんでふ','ありし・ありつる','例の','音に聞く','いかがせむ・いかがはせむ','さるべき','いざ給へ'];
-			meanings=['自然に/偶然に/万一','すぐに','早く/すでに','早く(～したい・～してほしい)/早くも','総じて/普通','いっそう','このように','あれこれと','そのように','そう','その程度・それほど/非常に','そのまま/全部','少しも～ない','まさか～ないだろう','かえって','そうはいってもやはり','一方では/すぐに','どうして(～か)/どうして(～か。いや、～ない)','どうして(～か)/どうして(～か。いや、～ない)','かつての/さっきの','いつものように/いつもの','うわさに聞く/評判が高い','どうしようか/どうしようもない','適当な/そうなるはずの/立派な','さあ、(一緒に)いらっしゃい'];
-		}
+		words=['領:し;る','あくがる','休らふ','うち出づ','やつす','音なふ','側:そば;む','語らふ','住む','障:さわ;る'];
+		meanings=['(土地を)領有する','さまよい出る/(魂が)宙にさまよう/(心が)うわの空になる','立ち止まる、とどまる/ためらう','口に出して言う','地味な格好にする','音を立てる/手紙を出す、訪れる','横を向く','交際する','(男が女のもとに)通う','妨げられる'];
+		words.push('饗:あるじ;す','もてなす','興:きよう;ず','およぐす','時めく','避:さ;る','困:こう;ず','掻き暗す','憂ふ・愁ふ','託:かこ;つ');
+		meanings.push('客にごちそうする','振る舞う/取り扱う/もてはやす','おもしろがる','成長する/大人びる','寵愛を受ける','避ける/断る','疲れる','悲しみが心を暗くする','訴える','不平を言う');
+		words.push('労:いたは;る','ためらふ','怠る','悩む','後る・遅る','したたむ');
+		meanings.push('病気になる/骨を折る/世話をする','静養する/気を静める','病気がよくなる','病気で苦しむ','先立たれる','処理する/用意する/取り締まる');
 		rew=[];
 		rem=[];
 		qNum=[0,words.length,''];
@@ -70,9 +66,32 @@
 		// console.log(selects.indexOf(''));
 		// console.log(selects);
 		document.getElementById('qNum').textContent=`${qNum[2]}Q.${qNum[0]}/${qNum[1]}`;
-		document.getElementById('question').innerHTML=words[rs[0]];
+		document.getElementById('question').innerHTML=ruby(words[rs[0]]);
 		for(let i=0; i<4; i++){
 			document.getElementsByClassName('select')[i].textContent=selects[i];
+		}
+	}
+
+	function ruby(q){
+		if(q.indexOf(':')>-1){
+			let fq='';
+			for(let i=0; i<q.length; i++){
+				if(q.charAt(i+1)==':'){
+					fq+=`<ruby><rb>${q.charAt(i)}</rb><rt>`;
+					for(let j=i+2; j<q.length; j++){
+						if(q.charAt(j)==';'){
+							fq+="</rt></ruby>";
+							i=j+1;
+							break;
+						}
+						fq+=q.charAt(j);
+					}
+				}
+				fq+=q.charAt(i);
+			}
+			return fq;
+		}else{
+			return q;
 		}
 	}
 
@@ -155,14 +174,15 @@
 		}
 	})
 
-	for(let i=0; i<3; i++){
-		document.getElementsByClassName('startButton')[i].addEventListener('click',()=>{
-			mode=['kd','n','f'][i];
-			document.getElementsByClassName('buttons')[0].classList.add('none');
-			document.getElementsByClassName('selects')[0].classList.remove('none');
-			document.getElementById('return').classList.remove('none');
-			reset();
-			newQuestion();
-		})
+	for(let i=0; i<2; i++){
+		if(document.getElementsByClassName('startButton')[i]!=null){
+			document.getElementsByClassName('startButton')[i].addEventListener('click',()=>{
+				document.getElementsByClassName('buttons')[0].classList.add('none');
+				document.getElementsByClassName('selects')[0].classList.remove('none');
+				document.getElementById('return').classList.remove('none');
+				reset();
+				newQuestion();
+			})
+		}
 	}
 }
