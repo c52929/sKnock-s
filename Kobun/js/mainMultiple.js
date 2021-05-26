ef5c7e3d3b14709244787d8a55e53bb9353591e1
#c52929 <tableknocker0@gmail.com>
2021-05-26 22:12:57 +0900

    Updated Kobun vocab

--- Kobun/js/mainMultiple.js
+++ Kobun/js/mainMultiple.js
@@ -2,15 +2,21 @@
 
 {
 	let words, meanings, rew, rem, allMeanings, hideMeanings;
-	let rs, selects, qNum, selected, jokyo, mode;
+	let rs, selects, qNum, selected, jokyo, mode, showRuby, getRuby;
 
 	function reset(){
-		words=['領:し;る','あくがる','休らふ','うち出づ','やつす','音なふ','側:そば;む','語らふ','住む','障:さわ;る'];
-		meanings=['(土地を)領有する','さまよい出る/(魂が)宙にさまよう/(心が)うわの空になる','立ち止まる、とどまる/ためらう','口に出して言う','地味な格好にする','音を立てる/手紙を出す、訪れる','横を向く','交際する','(男が女のもとに)通う','妨げられる'];
-		words.push('饗:あるじ;す','もてなす','興:きよう;ず','およぐす','時めく','避:さ;る','困:こう;ず','掻き暗す','憂ふ・愁ふ','託:かこ;つ');
-		meanings.push('客にごちそうする','振る舞う/取り扱う/もてはやす','おもしろがる','成長する/大人びる','寵愛を受ける','避ける/断る','疲れる','悲しみが心を暗くする','訴える','不平を言う');
-		words.push('労:いたは;る','ためらふ','怠る','悩む','後る・遅る','したたむ');
-		meanings.push('病気になる/骨を折る/世話をする','静養する/気を静める','病気がよくなる','病気で苦しむ','先立たれる','処理する/用意する/取り締まる');
+		// words=['領:し;る','あくがる','休らふ','うち出づ','やつす','音なふ','側:そば;む','語らふ','住む','障:さわ;る'];
+		// meanings=['(土地を)領有する','さまよい出る/(魂が)宙にさまよう/(心が)うわの空になる','立ち止まる、とどまる/ためらう','口に出して言う','地味な格好にする','音を立てる/手紙を出す、訪れる','横を向く','交際する','(男が女のもとに)通う','妨げられる'];
+		// words.push('饗:あるじ;す','もてなす','興:きよう;ず','およぐす','時めく','避:さ;る','困:こう;ず','掻き暗す','憂ふ・愁ふ','託:かこ;つ');
+		// meanings.push('客にごちそうする','振る舞う/取り扱う/もてはやす','おもしろがる','成長する/大人びる','寵愛を受ける','避ける/断る','疲れる','悲しみが心を暗くする','訴える','不平を言う');
+		// words.push('労:いたは;る','ためらふ','怠る','悩む','後る・遅る','したたむ');
+		// meanings.push('病気になる/骨を折る/世話をする','静養する/気を静める','病気がよくなる','病気で苦しむ','先立たれる','処理する/用意する/取り締まる');
+		words=['惜:あたら;し','かたじけなし','痴:をこ;がまし','心:こころ;憎:にく;し','賢:さか;し','好:す;き好:ず;きし','なまめかし','今:いま;めかし','しどけなし','つきづきし'];
+		meanings=['もったいない','おそれ多い/ありがたい','愚かしい','奥ゆかしい','かしこい/気が利いている/気がしっかりしている/こざかしい','好色めいている/風流だ','優美だ','現代風だ/華やかだ','無造作だ/乱れている','似つかわしい'];
+		words.push('なつかし','目:め;安:やす;し','心:こころ;安:やす;し','後:うし;ろめたし','心:こころ;苦:ぐる;し','まばゆし','恥:は;づかし','拙:つたな;し','こちたし','左:さ;右:う;無:な;し');
+		meanings.push('好ましい','見苦しくない','安心だ/気楽だ','心配だ','つらい/気の毒だ','見ていられない/恥ずかしい','(こちらが気後れするほど)立派だ','下手だ/劣っている/下品だ/不運だ','仰々しい','ためらわない、無造作だ');
+		words.push('所:ところ;狭:せ;し','慎:つつ;まし','まさなし','妬:ねた;し','まだし');
+		meanings.push('いっぱいだ/窮屈だ/おおげさだ','気がひける','よくない/見苦しい','しゃくにさわる','まだ時期が早い/不十分だ');
 		rew=[];
 		rem=[];
 		qNum=[0,words.length,''];
@@ -22,6 +28,7 @@
 		// console.log(words);
 		qNum[0]++;
 		jokyo=0;
+		showRuby=false;
 		selects=[];
 		selected=[false,false,false,false];
 		rs=[Math.floor(Math.random()*words.length),''];
@@ -66,34 +73,49 @@
 		// console.log(selects.indexOf(''));
 		// console.log(selects);
 		document.getElementById('qNum').textContent=`${qNum[2]}Q.${qNum[0]}/${qNum[1]}`;
-		document.getElementById('question').innerHTML=ruby(words[rs[0]]);
+		getRuby=ruby(words[rs[0]],showRuby);
+		document.getElementById('question').innerHTML=getRuby[0];
+		if(getRuby[1]){
+			document.getElementById('wall').classList.add('changeRubyAvailable');
+		}else{
+			document.getElementById('wall').classList.remove('changeRubyAvailable');
+		}
 		for(let i=0; i<4; i++){
 			document.getElementsByClassName('select')[i].textContent=selects[i];
 		}
 	}
 
-	function ruby(q){
+	function ruby(q,bool){
+		// bool=true;
 		if(q.indexOf(':')>-1){
 			let fq='';
 			for(let i=0; i<q.length; i++){
 				if(q.charAt(i+1)==':'){
-					fq+=`<ruby><rb>${q.charAt(i)}</rb><rt>`;
+					fq+=[`<ruby><rb>${q.charAt(i)}</rb><rt>`,''][[true,false].indexOf(bool)];
 					for(let j=i+2; j<q.length; j++){
 						if(q.charAt(j)==';'){
-							fq+="</rt></ruby>";
-							i=j+1;
+							fq+=["</rt></ruby>",''][[true,false].indexOf(bool)];
+							i=j;
 							break;
 						}
 						fq+=q.charAt(j);
 					}
+				}else{
+					fq+=q.charAt(i);
 				}
-				fq+=q.charAt(i);
 			}
-			return fq;
+			return [fq,true];
 		}else{
-			return q;
+			return [q,false];
 		}
 	}
+
+	document.getElementById('wall').addEventListener('click',()=>{
+		if(mode==1){
+			showRuby=[false,true][[true,false].indexOf(showRuby)];
+			document.getElementById('question').innerHTML=ruby(words[rs[0]],showRuby)[0];
+		}
+	})
 
 	for(let i=0; i<4; i++){
 		document.getElementsByClassName('select')[i].addEventListener('click',()=>{
@@ -166,23 +188,36 @@
 				}else{
 					document.getElementById('qNum').textContent='Congratulations!';
 					document.getElementById('question').textContent='天晴';
-					document.getElementsByClassName('selects')[0].classList.add('none');
+					document.getElementById('selects').classList.add('none');
 					document.getElementById('return').classList.add('none');
-					document.getElementsByClassName('buttons')[0].classList.remove('none');
+					document.getElementById('buttons').classList.remove('none');
 				}
 			}
 		}
 	})
 
-	for(let i=0; i<2; i++){
-		if(document.getElementsByClassName('startButton')[i]!=null){
-			document.getElementsByClassName('startButton')[i].addEventListener('click',()=>{
-				document.getElementsByClassName('buttons')[0].classList.add('none');
-				document.getElementsByClassName('selects')[0].classList.remove('none');
+	// for(let i=0; i<2; i++){
+	// 	if(document.getElementsByClassName('startButton')[i]!=null){
+	// 		document.getElementsByClassName('startButton')[i].addEventListener('click',()=>{
+	// 			document.getElementById('buttons').classList.add('none');
+	// 			document.getElementById('selects').classList.remove('none');
+	// 			document.getElementById('return').classList.remove('none');
+	// 			reset();
+	// 			newQuestion();
+	// 		})
+	// 	}
+	// }
+
+	// for(let i=0; i<2; i++){
+		// if(document.getElementsByClassName('startButton')[i]!=null){
+			document.getElementById('honoSel').addEventListener('click',()=>{
+				mode=1;
+				document.getElementById('buttons').classList.add('none');
+				document.getElementById('selects').classList.remove('none');
 				document.getElementById('return').classList.remove('none');
 				reset();
 				newQuestion();
 			})
-		}
-	}
+		// }
+	// }
 }
--- Kobun/js/mainSingular.js
+++ Kobun/js/mainSingular.js
@@ -1,27 +1,74 @@
 'use strict';
 
 {
-	let questions, answers;
+	let questions, answers, qNum=[0], r;
+	let sentences, meanings;
 
-	document.getElementById('honoSel').addEventListener('click',()=>{
-		document.getElementsByClassName('sellects')[0].classList.remove('none');
+	document.getElementById('honoWri').addEventListener('click',()=>{
+		document.getElementById('selects').classList.add('none');
 		QandA();
 	})
 
 	function QandA(){
-		document.getElementsByClassName('buttons')[0].classList.add('none');
+		document.getElementById('buttons').classList.add('none');
 		questions=[];
-		answers=[];
-		let sentence=['その<ruby><rb>郎等</rb><rt>らうどう</rt></ruby>を[召す]に、<ruby><rb>跡</rb><rt>あと</rt></ruby>をくらみて<ruby><rb>失</rb><rt>う</rt></ruby>せぬ'];
-		let mean=['その家来を[お{呼}(よ)び{寄}(よ)せになる]と、(その家来は)行方をくらませて消えてしまった'];
-		let r;
-		while(sentence.length>0){
-			r=Math.floor(Math.random()*sentence.length);
-			questions.push(sentence[r]);
-			answers.push(mean[r]);
-			sentence.splice(r,1);
-			mean.splice(r,1);
+		sentences=['その{郎等}らうどう;を[召す]に、{跡}あと;をくらみて{失}う;せぬ'];
+		meanings=['その家来を[お{呼}よ;び{寄}よ;せになる]と、(その家来は)行方をくらませて消えてしまった'];
+		reformQs();
+		qNum.push(sentences.length);
+		while(sentences.length>0){
+			answers=[];
+			newQ();
+			sentences.splice(r,1);
+			meanings.splice(r,1);
 		}
+	}
+
+	function reformQs(){
+		let cp;
+		for(let i=0; i<sentences.length; i++){
+			cp=`${sentences[i]}。`;
+			sentences[i]='';
+			meanings[i]='';
+			answers.push('');
+			for(let j=0; j<cp.length; j++){
+				if(cp.charAt(j)=='{'){
+					sentences[i]+="<ruby><rb>";
+					for(let k=j+1; k<cp.length; k++){
+						if(cp.charAt(k)=='}'){
+							sentences[i]+="</rb><rt>";
+							k++;
+						}else if(cp.charAt(k)==';'){
+							sentences[i]+="</rt></ruby>";
+							j=k+1;
+							break;
+						}
+						sentences[i]+=cp.charAt(k);
+					}
+				}else if(cp.charAt(j)=='['){
+					sentences[i]+='<span class="qPoint">';
+					for(let k=j+1; k<cp.length; k++){
+						if(cp.charAt(k)==']'){
+							sentences[i]+='</span>';
+							j=k+1;
+							break;
+						}
+						sentences[i]+=cp.charAt(k);
+					}
+				}
+				sentences[i]+=cp.charAt(j);
+			}
+		}
+
+	}
+
+	function newQ(){
+		qNum[0]++;
+		r=Math.floor(Math.random()*sentences.length);
+		// questions.push(sentences[r]);
+		// answers.push(meanings[r]);
+		document.getElementById('qNum').textContent=`Q.${qNum[0]}/${qNum[1]}`;
+		document.getElementById('question').innerHTML=sentences[r];
 	}
 
 }
